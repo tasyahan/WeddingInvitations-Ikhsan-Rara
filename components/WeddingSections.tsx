d35291ce967951580d8ba5eeb0af7055b61a0c3a
tasyahan <tasssyahan@gmail.com>
2026-04-10 15:38:09 +0700

    feat: brightness for couple asset

--- components/WeddingSections.tsx
+++ components/WeddingSections.tsx
@@ -93,7 +93,7 @@ export const CoupleSection = () => {
         <img
           src={ASSETS.couple.main}
           alt="The Happy Couple"
-          className="absolute inset-0 w-full h-full object-cover object-top scale-x-[-1]"
+          className="absolute inset-0 w-full h-full object-cover object-top scale-x-[-1] brightness-150 sm:brightness-100"
         />
 
         {/* Overlays for readability - adjusted opacity */}
